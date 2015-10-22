85bdb061ea7e08fe567eea6413211644cf567197
marcuscop <marcdreardon@gmail.com>
2015-10-22 18:47:00 -0400

    I added some stuff to discover how much you guys know
    ya

--- meg_code.c
+++ meg_code.c
@@ -1,8 +1,32 @@
 
-task main()
-{
+/*
+If you have come accross this, GOOD JOB!
+You have successfully set up github at home!
+Try to explain what this function does:
+(in comments like this)
+*/
 
-//hiojngijfdng
+void function_(float final_speed){
+	int speed = 0;
+	while (speed < final_speed){
+		motor[flyr] = speed;
+		motor[flyl] = -speed;
+		speed = speed + 10;
+		wait1Msec(300);
 
-
+	}
+		
 }
+
+
+/* comment in here:
+
+
+
+
+
+
+
+
+
+*/
