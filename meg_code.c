da14aa0134cfcd8fb7159c4a29bc39e1e6cd467b
chlorophyllbabe <meaghan.silverman@gmail.com>
2015-10-27 16:45:56 -0400

    Marc thing
    blah

--- meg_code.c
+++ meg_code.c
@@ -1,8 +1,22 @@
+/*
+If you have come accross this, GOOD JOB!
+You have successfully set up github at home!
+Try to explain what this function does:
+(in comments like this)
+*/
 
-task main()
-{
+void function_(float final_speed){
+	int speed = 0;
+	while (speed < final_speed){
+		motor[flyr] = speed;
+		motor[flyl] = -speed;
+		speed = speed + 10;
+		wait1Msec(300);
 
-//hiojngijfdng
-
-
+	}
+		
 }
+
+
+/* comment in here:
+*/It makes the fly wheel, I'm guessing, move +10 after waiting 3 seconds
